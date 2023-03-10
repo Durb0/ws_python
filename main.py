e26f2c1d37eb494457c17c03040818f5e671af96
Durb0 <58364246+Durb0@users.noreply.github.com>
2023-03-10 20:42:19 +0100

    oskour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from wsgiref.simple_server import make_server
 from application import wsgi_application
 
-server = make_server('localhost', 8000, wsgi_application)
-server.serve_forever()
+app = wsgi_application()
