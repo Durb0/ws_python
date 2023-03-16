a548e3d4a5333582330b761617bbfe124563f99a
Durb0 <thomasnou@orange.fr>
2023-03-16 11:27:02 +0100

    new algo

--- utils.py
+++ utils.py
@@ -6,13 +6,18 @@ import requests
 
 def get_shortest_path(start: Tuple[float, float], end: Tuple[float, float], max_distance: float) \
         -> List[Tuple[float, float]]:
+    def get_center(pointA: Tuple[float, float], pointB: Tuple[float, float]) -> Tuple[float, float]:
+        return ((pointA[0] + pointB[0]) / 2, (pointA[1] + pointB[1]) / 2)
 
-    def get_charging_stations_around(center: Tuple[float, float]) -> List[Tuple[float, float]]:
+    def get_distance(pointA: Tuple[float, float], pointB: Tuple[float, float]) -> float:
+        return geopy.distance.distance(pointA, pointB).km
+
+    def get_charging_stations_around(center: Tuple[float, float], radius) -> List[Tuple[float, float]]:
         apiUrl = 'https://odre.opendatasoft.com/api/records/1.0/search/'
         params = {
             'dataset': 'bornes-irve',
-            'rows': 5000,
-            'geofilter.distance': '%s,%s,%s' % (center[0], center[1], max_distance*1000)
+            'rows': 10000,
+            'geofilter.distance': '%s,%s,%s' % (center[0], center[1], radius * 1000)
         }
         response = requests.get(apiUrl, params=params, timeout=30000)
         data = response.json()
@@ -32,39 +37,35 @@ def get_shortest_path(start: Tuple[float, float], end: Tuple[float, float], max_
                 shortest_coord = coord
         return shortest_coord
 
-    def get_coords_around(center: Tuple[float, float], coords: List[Tuple[float, float]]) -> List[
+    def get_borns_around(center: Tuple[float, float]) -> List[
         Tuple[float, float]]:
         coords_around = []
-        for coord in coords:
+        for coord in charging_stations:
             distance = geopy.distance.distance(center, coord).km
             if distance < max_distance:
                 coords_around.append(coord)
         return coords_around
 
-    list = []
-    # il faut faire une recursivité pour trouver le chemin le plus court
-    # si la distance entre le point de départ et le point d'arrivée est inférieur à la distance max
-    # alors on retourne le point d'arrivée
-    # sinon on cherche l'ensemble des points de charge autour du point de départ
-    # on cherche le point de charge le plus proche du point d'arrivée
-    # on rappelle la fonction avec le point de charge le plus proche comme point de départ
-    # et le point d'arrivée comme point d'arrivée
+    charging_stations = get_charging_stations_around(get_center(start, end), get_distance(start, end) / 2)
 
-    list.append(start)
-    distance = geopy.distance.distance(start, end).km
-    if distance < max_distance:
-        list.append(end)
-        return list
-    else:
-        charging_stations = get_charging_stations_around(start)
-        coords_around = get_coords_around(start, charging_stations)
-        # si il n'y a pas de point de charge autour du point de départ
-        # alors on retourne le point d'arrivée
-        if len(coords_around) == 0:
-            list.append(end)
-            return list
-        shortest_coord = get_shortest_coord(end, coords_around)
-        # si il n'y a pas de point de charge autour du point de départ
-        # alors on retourne le point d'arrivée
-        list.extend(get_shortest_path(shortest_coord, end, max_distance))
-        return list
+    res_list = [start]
+
+    while res_list[-1] != end:
+        print(res_list)
+        if get_distance(res_list[-1], end) < max_distance:
+            res_list.append(end)
+            break
+        borns_around = get_borns_around(res_list[-1])
+        if len(borns_around) == 0:
+            res_list.append(end)
+            break
+        shortest_coord = get_shortest_coord(end, borns_around)
+        if shortest_coord is None:
+            res_list.append(end)
+            break
+        if shortest_coord == res_list[-1]:
+            res_list.append(end)
+            break
+        res_list.append(shortest_coord)
+
+    return res_list
